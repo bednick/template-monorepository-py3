1d9d54538c0bb226225f40811464b36b61bda978
Bedarev Nikolay <nikolay.bedarev@indriver.com>
2024-08-19 17:24:31 +0300

    add alembic

--- services/example-service/tests_example_workers/conftest.py
+++ services/example-service/tests_example_workers/conftest.py
@@ -1,7 +1,12 @@
+import os
+import pathlib
 from typing import Generator
 
+import alembic.command
+import alembic.config
 import fastapi.testclient
 import pytest
+import sqlalchemy_utils
 
 from example_service import application, config, database
 
@@ -11,6 +16,7 @@ def settings() -> config.Settings:
     return config.Settings(
         database=database.config.Settings(
             DATABASE_POSTGRESQL_DATABASE_NAME="example-service-tests",
+            DATABASE_POSTGRESQL_POOL_SIZE=-1,
         ),
     )
 
@@ -23,7 +29,24 @@ def app(settings: config.Settings) -> fastapi.FastAPI:
     return app
 
 
+@pytest.fixture(scope="package", autouse=True)
+def init_database(settings: config.Settings):
+    """Init test database (create DB, apply migrations)"""
+    dsn = settings.database.sync_dsn
+    if sqlalchemy_utils.database_exists(dsn):
+        sqlalchemy_utils.drop_database(dsn)
+    sqlalchemy_utils.create_database(dsn)
+    try:
+        os.environ["DATABASE_POSTGRESQL_DATABASE_NAME"] = settings.database.database_name
+        alembic_cfg = alembic.config.Config(pathlib.Path(__file__).parent.parent / "alembic.ini")
+        alembic.command.upgrade(alembic_cfg, "head")
+        yield
+    finally:
+        if sqlalchemy_utils.database_exists(dsn):
+            sqlalchemy_utils.drop_database(dsn)
+
+
 @pytest.fixture()
-def client(app: fastapi.FastAPI) -> Generator[fastapi.testclient.TestClient, None, None]:
+def client(app: fastapi.FastAPI, init_database) -> Generator[fastapi.testclient.TestClient, None, None]:
     with fastapi.testclient.TestClient(app) as client:
         yield client
